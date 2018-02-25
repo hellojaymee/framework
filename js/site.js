0101e82fea86e646494cbfad0bbe9ddf7176611d
Jaymee Robertson <jaymee.robertson@gmail.com>
2018-02-25 08:15:55 -0600

    add framework

---
+++ js/site.js
@@ -0,0 +1,16 @@
+// =============================================================================
+//	Imports
+// =============================================================================
+//= include components/updatevalue.js
+//= include components/backgroundImage.js
+//= include components/bodyClassToggler.js
+//= include components/accordianMenu.js
+//= include components/navChecker.js
+//= include components/textWrapper.js
+
+
+$(document).ready(function() {
+
+	console.log("hello world");
+
+}); // end document ready
